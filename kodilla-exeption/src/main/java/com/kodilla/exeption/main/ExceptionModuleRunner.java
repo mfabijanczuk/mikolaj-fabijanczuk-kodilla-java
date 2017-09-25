1733a3e9529c4f78dd2bda4a06d2031f91f11ea6
“mfabijanczuk <mikolaj.fabijanczuk@gmail.com>
2017-09-25 02:49:39 +0200

    Moduł 8.2

---
+++ kodilla-exeption/src/main/java/com/kodilla/exeption/main/ExceptionModuleRunner.java
@@ -0,0 +1,11 @@
+package com.kodilla.exeption.main;
+
+import com.kodilla.exeption.io.FileReader;
+
+public class ExceptionModuleRunner {
+
+    public static void main(String args[]){
+            FileReader fileReader = new FileReader();
+            fileReader.readFile();
+    }
+}
