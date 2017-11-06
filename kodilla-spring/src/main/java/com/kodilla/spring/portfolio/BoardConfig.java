147d9f51e51e572aae46fd22c795d7013d3fb780
“mfabijanczuk <mikolaj.fabijanczuk@gmail.com>
2017-11-06 20:34:28 +0100

    Moduł 10.4

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,11 +1,14 @@
 package com.kodilla.spring.portfolio;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
+    @Autowired
+    Board board;
 
     @Bean(name = "toDoList")
     @Scope("prototype")
