37573fb5184068d3fdb4811ba3b4897c39fc7977
“mfabijanczuk <mikolaj.fabijanczuk@gmail.com>
2018-01-07 23:27:41 +0100

    Moduł 22.3.

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
@@ -0,0 +1,56 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class StoredPostTestSuite {
+    @Test
+    public void testUpdateVipLevels() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        statement.execute(sqlProcedureCall);
+
+        //Then
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+        ResultSet rs = statement.executeQuery(sqlCheckTable);
+        int howMany = -1;
+        if(rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
+        }
+        assertEquals(0, howMany);
+    }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        //Given
+        DbManager dbManager = DbManager.getInstance();
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = FALSE";
+        Statement statement = dbManager.getConnection().createStatement();
+        statement.executeUpdate(sqlUpdate);
+
+        //When
+        String sqlUpdateBestsellers = "CALL UpdateBestsellers()";
+        statement.execute(sqlUpdateBestsellers);
+
+        //Then
+        String sqlCheckUpdate = "SELECT COUNT(*) AS HOW_MANY FROM BOOKS WHERE BESTSELLER = TRUE";
+        ResultSet st = statement.executeQuery(sqlCheckUpdate);
+        int howMany = -1;
+        if(st.next()) {
+            howMany = st.getByte("HOW_MANY");
+        }
+        assertEquals(2, howMany);
+    }
+
+}
